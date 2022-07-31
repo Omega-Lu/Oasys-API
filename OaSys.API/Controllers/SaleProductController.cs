54cdc955dfb4d00b6d255844d5c636689fc5d5dd
Omega-Lu <69754655+Omega-Lu@users.noreply.github.com>
2022-07-31 19:49:24 +0200

    SaleProduct

--- OaSys.API/Controllers/SaleProductController.cs
+++ OaSys.API/Controllers/SaleProductController.cs
@@ -10,5 +10,75 @@ namespace OaSys.API.Controllers
     [ApiController]
     public class SaleProductController : ControllerBase
     {
+        public readonly OasysDBContext saleProductDBContext;
+
+        public SaleProductController(OasysDBContext saleProductDBContext)
+        {
+            this.saleProductDBContext = saleProductDBContext;
+        }
+        //get All Saleproducts
+        [HttpGet]
+
+        public async Task<IActionResult> GetAllSaleProducts()
+        {
+            var saleProduct = await saleProductDBContext.SaleProduct.ToListAsync();
+            return Ok(saleProduct);
+        }
+
+        //Get Single SaleProduct
+        [HttpGet]
+
+        [HttpGet("{id}")]
+        [ActionName("GetSaleProduct")]
+        public async Task<IActionResult> GetSaleProduct(int id)
+        {
+            var saleProducts = await saleProductDBContext.SaleProduct.FirstOrDefaultAsync(x => x.PRODUCT_ID == id);
+            if (saleProducts != null)
+            {
+                return Ok(saleProducts);
+            }
+            return NotFound("SaleProduct not found");
+        }
+
+        //add single SaleProducts
+        [HttpPost]
+        public async Task<IActionResult> AddSaleProduct([FromBody] SaleProduct saleProduct)
+        {
+            await saleProductDBContext.SaleProduct.AddAsync(saleProduct);
+            await saleProductDBContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetSaleProduct), new { id = saleProduct.PRODUCT_ID }, saleProduct);
+        }
+
+        //Updating a SaleProducts
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOrderProduct(int id, [FromBody] SaleProduct saleProduct)
+        {
+            var existingSaleProduct = await saleProductDBContext.SaleProduct.FirstOrDefaultAsync(x => x.PRODUCT_ID == id);
+            if (existingSaleProduct != null)
+            {
+                existingSaleProduct.PRODUCT_ID = id;
+                existingSaleProduct.SALE_ID = saleProduct.SALE_ID;
+                existingSaleProduct.QUANTITY = saleProduct.QUANTITY;
+                await saleProductDBContext.SaveChangesAsync();
+                return Ok(existingSaleProduct);
+            }
+            return NotFound("SaleProduct Not Found");
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var existingSaleProduct = await saleProductDBContext.SaleProduct.FirstOrDefaultAsync(x => x.PRODUCT_ID == id);
+            if (existingSaleProduct != null)
+            {
+                saleProductDBContext.SaleProduct.Remove(existingSaleProduct);
+                await saleProductDBContext.SaveChangesAsync();
+                return Ok(existingSaleProduct);
+            }
+            return NotFound("SaleProduct not found");
+        }
     }
+
+    
 }
